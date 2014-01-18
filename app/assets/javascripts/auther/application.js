532b60e0f668e40e1779fea87db9a70c2b2d7a59
Brooke Kuhlmann <brooke@redalchemist.com>
2014-01-18 11:52:10 -0700

    Added Zurb Foundation support.
    * Greatly cleans up the authorization UI.

---
+++ app/assets/javascripts/auther/application.js
@@ -0,0 +1,5 @@
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+
+$(function(){ $(document).foundation(); });
--- lib/auther/engine.rb
+++ lib/auther/engine.rb
@@ -5,6 +5,13 @@ module Auther
     # Set defaults. Can be overwritten in app config.
     config.auther_settings = {}
 
+    # Add jQuery assets.
+    config.assets.paths << "#{Gem.loaded_specs['jquery-rails'].full_gem_path}/vendor/assets/javascripts"
+
+    # Add Zurb Foundation assets.
+    config.assets.paths << "#{Gem.loaded_specs['foundation-rails'].full_gem_path}/vendor/assets/stylesheets"
+    config.assets.paths << "#{Gem.loaded_specs['foundation-rails'].full_gem_path}/vendor/assets/javascripts"
+
     initializer "auther.initialize" do |app|
       # Initialize Gatekeeper middleware.
       app.config.app_middleware.use Auther::Gatekeeper, app.config.auther_settings
