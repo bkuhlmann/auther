41210168528c991b32b1d399ed584e2098e96d97
Brooke Kuhlmann <brooke@redalchemist.com>
2014-01-19 17:11:34 -0700

    Added login and password log filter parameters.

--- lib/auther/engine.rb
+++ lib/auther/engine.rb
@@ -6,13 +6,18 @@ module Auther
     config.auther_settings = {}
 
     # Add jQuery assets.
-    config.assets.paths << "#{Gem.loaded_specs['jquery-rails'].full_gem_path}/vendor/assets/javascripts"
+    jquery_gem_path = Gem.loaded_specs["jquery-rails"].full_gem_path
+    config.assets.paths << "#{jquery_gem_path}/vendor/assets/javascripts"
 
     # Add Zurb Foundation assets.
-    config.assets.paths << "#{Gem.loaded_specs['foundation-rails'].full_gem_path}/vendor/assets/stylesheets"
-    config.assets.paths << "#{Gem.loaded_specs['foundation-rails'].full_gem_path}/vendor/assets/javascripts"
+    foundation_gem_path = Gem.loaded_specs["foundation-rails"].full_gem_path
+    config.assets.paths << "#{foundation_gem_path}/vendor/assets/stylesheets"
+    config.assets.paths << "#{foundation_gem_path}/vendor/assets/javascripts"
 
     initializer "auther.initialize" do |app|
+      # Configure log filter parameters.
+      app.config.filter_parameters += [:login, :password]
+
       # Initialize Gatekeeper middleware.
       app.config.app_middleware.use Auther::Gatekeeper, app.config.auther_settings
     end
