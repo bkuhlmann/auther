e0a8f9cb48d381f64889c3a3d738bfb400647544
Brooke Kuhlmann <brooke@redalchemist.com>
2014-04-07 20:14:32 -0600

    Fixed bug with engine assets not being loaded properly within engine initializer.

--- lib/auther/engine.rb
+++ lib/auther/engine.rb
@@ -5,16 +5,16 @@ module Auther
     # Set defaults. Can be overwritten in app config.
     config.auther_settings = {}
 
-    # Add jQuery assets.
-    jquery_gem_path = Gem.loaded_specs["jquery-rails"].full_gem_path
-    config.assets.paths << "#{jquery_gem_path}/vendor/assets/javascripts"
-
-    # Add Zurb Foundation assets.
-    foundation_gem_path = Gem.loaded_specs["foundation-rails"].full_gem_path
-    config.assets.paths << "#{foundation_gem_path}/vendor/assets/stylesheets"
-    config.assets.paths << "#{foundation_gem_path}/vendor/assets/javascripts"
-
     initializer "auther.initialize" do |app|
+      # Add jQuery assets.
+      jquery_gem_path = Gem.loaded_specs["jquery-rails"].full_gem_path
+      app.config.assets.paths << "#{jquery_gem_path}/vendor/assets/javascripts"
+
+      # Add Zurb Foundation assets.
+      foundation_gem_path = Gem.loaded_specs["foundation-rails"].full_gem_path
+      app.config.assets.paths << "#{foundation_gem_path}/vendor/assets/stylesheets"
+      app.config.assets.paths << "#{foundation_gem_path}/vendor/assets/javascripts"
+
       # Configure log filter parameters.
       app.config.filter_parameters += [:login, :password]
 
