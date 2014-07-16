ad5d6b40bb789f12064e0d7692d73fad659ba120
Brooke Kuhlmann <brooke@redalchemist.com>
2014-07-15 20:29:57 -0600

    Updated Dummy application for specs to Rails 4.1.x settings.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,8 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
+# require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_view/railtie"
 require "sprockets/railtie"
 
 Bundler.require
@@ -13,3 +14,4 @@ module Dummy
     config.assets.compile = false
   end
 end
+
