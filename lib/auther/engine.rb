a84b4d6057666077adf6f3fddea0aa945d49ac2a
Brooke Kuhlmann <brooke@alchemists.io>
2015-11-10 19:46:31 -0700

    Updated engine to ignore modifying the DOM when errors are detected.
    - Disables default Rails ActionView configuration from adding
      additional DOM tags and classes for form errors.
    - Caveat: This setting can be trumped by the host application
      configuration. This can be good and bad:
        - Good: If no additional wrapper tags are used and only
          attributes are modified.
        - Bad: If wrapper tags are used then authentication form
          view layout will not render properly.

--- lib/auther/engine.rb
+++ lib/auther/engine.rb
@@ -4,6 +4,7 @@ module Auther
     isolate_namespace Auther
 
     config.auther_settings = {}
+    config.action_view.field_error_proc = proc { |html_tag, _| html_tag.html_safe }
 
     config.to_prepare do
       Dir.glob(Engine.root + "app/presenters/**/*.rb").each do |presenter|
@@ -12,7 +13,7 @@ module Auther
     end
 
     initializer "auther.initialize" do |app|
-      app.config.app_middleware.use Auther::Gatekeeper, app.config.auther_settings
+      app.config.app_middleware.use Gatekeeper, app.config.auther_settings
       app.config.filter_parameters += [:login, :password]
     end
 
