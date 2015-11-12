1036f402df0bc9d08b6ed5d1df46c2f731e80933
Brooke Kuhlmann <brooke@alchemists.io>
2015-11-11 19:45:21 -0700

    Fixed issue with Bourbon/Neat not loading for apps that don't require them.
    - Necesary for apps that don't normally require Bourbon or Neat
      so that these gems and, especially, their associated resources
      are loaded for use by the host app.

--- lib/auther/engine.rb
+++ lib/auther/engine.rb
@@ -1,3 +1,6 @@
+require "bourbon"
+require "neat"
+
 module Auther
   # The main engine.
   class Engine < ::Rails::Engine
@@ -7,20 +10,12 @@ module Auther
     config.action_view.field_error_proc = proc { |html_tag, _| html_tag.html_safe }
 
     config.to_prepare do
-      Dir.glob(Engine.root + "app/presenters/**/*.rb").each do |presenter|
-        require_dependency presenter
-      end
+      Dir.glob(Engine.root + "app/presenters/**/*.rb").each { |presenter| require_dependency presenter }
     end
 
     initializer "auther.initialize" do |app|
       app.config.app_middleware.use Gatekeeper, app.config.auther_settings
       app.config.filter_parameters += [:login, :password]
-    end
-
-    private
-
-    def full_gem_path name
-      Gem.loaded_specs[name].full_gem_path
     end
   end
 end
