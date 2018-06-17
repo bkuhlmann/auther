5c7956483c94a9ae2d616ce6adc24927c3aa7e72
Brooke Kuhlmann <brooke@alchemists.io>
2018-06-17 11:19:32 -0600

    Added cipher credentials generator.
    Given a login and password, the `Auther::Cipher` will generate
    credentials for secret, login, and password which can be used within a
    Rails application.
    
    Usage:
    
        Auther::Cipher.generate "test", "example"
    
    Output:
    
        {
          secret: "28f5fe96861e61031572db530644e1de",
          login: "0FeSjryd80QRVtGGRM4=--KbHXLTcGue9CxNwg...",
          password: "NjnhH06wi5W/7vdYVKDAoEs=--6mnaQcEKJ..."
        }

--- lib/auther/cipher.rb
+++ lib/auther/cipher.rb
@@ -3,6 +3,23 @@
 module Auther
   # Manages encryption/decryption.
   class Cipher
+    BYTE_DIVISOR = 2
+
+    def self.generate login, password
+      secret = SecureRandom.hex key_length / BYTE_DIVISOR
+      cipher = new secret
+
+      {
+        secret: secret,
+        login: cipher.encrypt(login),
+        password: cipher.encrypt(password)
+      }
+    end
+
+    def self.key_length
+      ActiveSupport::MessageEncryptor.key_len
+    end
+
     def initialize secret
       @encryptor = ActiveSupport::MessageEncryptor.new secret
     end
--- spec/lib/auther/cipher_spec.rb
+++ spec/lib/auther/cipher_spec.rb
@@ -4,11 +4,27 @@ require "rails_helper"
 require "securerandom"
 
 RSpec.describe Auther::Cipher do
-  let(:secret) { SecureRandom.random_bytes 32 }
+  let(:secret) { SecureRandom.random_bytes ActiveSupport::MessageEncryptor.key_len }
   let(:encryptor) { ActiveSupport::MessageEncryptor.new secret }
   let(:encrypted_data) { encryptor.encrypt_and_sign "password" }
   let(:decrypted_data) { "password" }
   subject { Auther::Cipher.new secret }
+
+  describe ".generate" do
+    it "answers credentials" do
+      expect(described_class.generate("test", "password")).to match(
+        secret: match(/\A[0-9a-f]{#{described_class.key_length}}\Z/),
+        login: match(%r(\A[0-9a-zA-Z\=\-\+\/]{110}\Z)),
+        password: match(%r(\A[0-9a-zA-Z\=\-\+\/]{138}\Z))
+      )
+    end
+  end
+
+  describe ".key_length" do
+    it "answers key length" do
+      expect(described_class.key_length).to eq(32)
+    end
+  end
 
   describe "#encrypt" do
     it "encrypts data" do
