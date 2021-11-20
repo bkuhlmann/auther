ad7e849f9194fcc23d66895cc29d4d31b4d04652
Brooke Kuhlmann <brooke@alchemists.io>
2021-11-20 11:49:54 -0700

    Added version release notes

--- lib/auther/identity.rb
+++ lib/auther/identity.rb
@@ -5,7 +5,7 @@ module Auther
   module Identity
     NAME = "auther"
     LABEL = "Auther"
-    VERSION = "12.1.0"
+    VERSION = "12.2.0"
     VERSION_LABEL = "#{LABEL} #{VERSION}".freeze
   end
 end
