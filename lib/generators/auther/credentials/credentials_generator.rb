a021c6c890bcc82d4afdef7fb872d9a8a1db01cd
Brooke Kuhlmann <brooke@alchemists.io>
2022-06-27 10:30:59 -0600

    Fixed RuboCop Layout/LineContinuationLeadingSpace
    Necessary remove leading white space. The Reek violation was also
    resolved by moving the information dump to a private method.

--- lib/generators/auther/credentials/credentials_generator.rb
+++ lib/generators/auther/credentials/credentials_generator.rb
@@ -4,20 +4,26 @@ module Auther
   # Credentials generator for new secret, login, and password.
   class CredentialsGenerator < ::Rails::Generators::Base
     desc "Generate Auther secret, login, and password credentials."
-    # :reek:TooManyStatements
     def credentials
       puts "Welcome to the Auther credentials generator.\n"
 
       login = ask "  Enter admin login:", echo: false
       password = ask "\n  Enter admin password:", echo: false
-      credentials = Cipher.generate login, password
+      identity = Cipher.generate login, password
 
+      display identity
+    end
+
+    private
+
+    def display identity
       puts "\n\nHere are your credentials:\n"
 
-      say "  AUTHER_SECRET=#{credentials.fetch :secret}\n" \
-          "  AUTHER_ADMIN_LOGIN=#{credentials.fetch :login}\n" \
-          "  AUTHER_ADMIN_PASSWORD=#{credentials.fetch :password}",
-          :green
+      say <<~MESSAGE.gsub(/^/, "  "), :green
+        AUTHER_SECRET=#{identity.fetch :secret}
+        AUTHER_ADMIN_LOGIN=#{identity.fetch :login}
+        AUTHER_ADMIN_PASSWORD=#{identity.fetch :password}
+      MESSAGE
 
       say "\nReminder: Do not add these credentials to source control.", :yellow
     end
